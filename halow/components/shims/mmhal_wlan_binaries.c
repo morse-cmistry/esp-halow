2e3a76a562c11b929046ab3d3cf3f1a35b958514
Michael McCandless <michael.mccandless@morsemicro.com>
2026-06-12 00:41:34 +0000

    firmware: absorb firmware component into halow component
    Separated components lead to work arounds in CMake to attempt addressing
    structural differences in development.
    Switch blob inclusion to target_add_binary_data
    
    Tracked-by: OSP-676
    Signed-off-by: Michael McCandless <michael.mccandless@morsemicro.com>

--- halow/components/shims/mmhal_wlan_binaries.c
+++ halow/components/shims/mmhal_wlan_binaries.c
@@ -18,12 +18,12 @@
 
 void mmhal_wlan_read_bcf_file(uint32_t offset, uint32_t requested_len, struct mmhal_robuf *robuf)
 {
-    /** Points to the start of the BCF binary image. Defined as part of the Makefile */
-    extern uint8_t bcf_binary_start;
-    /** Points to the end of the BCF binary image. Defined as part of the Makefile */
-    extern uint8_t bcf_binary_end;
+    /** Points to the start of the BCF binary image. Embedded by the firmware CMake */
+    extern const uint8_t bcf_binary[];
+    /** Length of the BCF binary image. Embedded by the firmware CMake */
+    extern const uint32_t bcf_binary_length;
 
-    size_t bcf_len = &bcf_binary_end - &bcf_binary_start;
+    size_t bcf_len = bcf_binary_length;
 
     /* Initialise robuf */
     robuf->buf = NULL;
@@ -32,13 +32,13 @@ void mmhal_wlan_read_bcf_file(uint32_t offset, uint32_t requested_len, struct mm
     robuf->free_cb = NULL;
 
     /* Sanity check */
-    if (bcf_len < offset)
+    if (bcf_binary_length < offset)
     {
         printf("Detected an attempt to start reading off the end of the bcf file.\n");
         return;
     }
 
-    robuf->buf = (uint8_t *)&bcf_binary_start + offset;
+    robuf->buf = (uint8_t *)bcf_binary + offset;
     robuf->len = bcf_len - offset;
     robuf->len = (robuf->len < requested_len) ? robuf->len : requested_len;
 }
@@ -48,22 +48,22 @@ void mmhal_wlan_read_bcf_file(uint32_t offset, uint32_t requested_len, struct mm
  *                                    Firmware Retrieval
  * ---------------------------------------------------------------------------------------------
  */
-/** Points to the start of the firmware binary image. Defined as part of the Makefile */
-extern uint8_t firmware_binary_start;
-/** Points to the end of the firmware binary image. Defined as part of the Makefile */
-extern uint8_t firmware_binary_end;
+/** Points to the start of the firmware binary image. Embedded by the firmware CMake */
+extern const uint8_t firmware_binary[];
+/** Points to the end of the firmware binary image. Embedded by the firmware CMake */
+extern const uint32_t firmware_binary_length;
 
 void mmhal_wlan_read_fw_file(uint32_t offset, uint32_t requested_len, struct mmhal_robuf *robuf)
 {
-    uint32_t firmware_len = &firmware_binary_end - &firmware_binary_start;
-    if (offset > firmware_len)
+    uint32_t firmware_len = firmware_binary_length;
+    if (offset > firmware_binary_length)
     {
         printf("Detected an attempt to start read off the end of the firmware file.\n");
         robuf->buf = NULL;
         return;
     }
 
-    robuf->buf = (&firmware_binary_start + offset);
+    robuf->buf = (uint8_t *)firmware_binary + offset;
     firmware_len -= offset;
 
     robuf->len = (firmware_len < requested_len) ? firmware_len : requested_len;
